e1e1829e58f0e59b4f9c2bea0e80ecaae63dd75b
nukosuke <nukosuke@lavabit.com>
2018-10-25 01:32:59 +0900

    Fix footer and header

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -13,24 +13,18 @@ const Footer = (props) => (
             </ul>
         </section>
         <section>
-            <h2>Etiam feugiat</h2>
+            <h2>Info</h2>
             <dl className="alt">
-                <dt>Address</dt>
-                <dd>1234 Somewhere Road &bull; Nashville, TN 00000 &bull; USA</dd>
-                <dt>Phone</dt>
-                <dd>(000) 000-0000 x 0000</dd>
-                <dt>Email</dt>
-                <dd><a href="#">information@untitled.tld</a></dd>
+                <dt>Workplace</dt>
+                <dd>Shibuya &bull; Tokyo &bull; Japan</dd>
             </dl>
             <ul className="icons">
-                <li><a href="#" className="icon fa-twitter alt"><span className="label">Twitter</span></a></li>
-                <li><a href="#" className="icon fa-facebook alt"><span className="label">Facebook</span></a></li>
-                <li><a href="#" className="icon fa-instagram alt"><span className="label">Instagram</span></a></li>
-                <li><a href="#" className="icon fa-github alt"><span className="label">GitHub</span></a></li>
-                <li><a href="#" className="icon fa-dribbble alt"><span className="label">Dribbble</span></a></li>
+                <li><a href="https://twitter.com/pg_nukosuke" className="icon fa-twitter alt"><span className="label">Twitter</span></a></li>
+                <li><a href="https://github.com/nukosuke" className="icon fa-github alt"><span className="label">GitHub</span></a></li>
+                <li><a href="https://medium.com/@nukosuke" className="icon fa-medium alt"><span className="label">Medium</span></a></li>
             </ul>
         </section>
-        <p className="copyright">&copy; Untitled. Design: <a href="https://html5up.net">HTML5 UP</a>.</p>
+        <p className="copyright">&copy; nukosuke.com. Designed by <a href="https://html5up.net">HTML5 UP</a>.</p>
     </footer>
 )
 
