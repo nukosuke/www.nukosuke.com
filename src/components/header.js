c2d384f5ad4da39baa09be0eab3a53e21eea5406
nukosuke <nukosuke@lavabit.com>
2018-10-25 00:34:52 +0900

    Use template

--- src/components/header.js
+++ src/components/header.js
@@ -1,33 +1,14 @@
 import React from 'react'
-import { Link } from 'gatsby'
 
-const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </div>
+import logo from '../assets/images/logo.svg';
+
+const Header = (props) => (
+    <header id="header" className="alt">
+        <span className="logo"><img src={logo} alt="" /></span>
+        <h1>Stellar</h1>
+        <p>Just another free, fully responsive site template<br />
+        design by <a href="https://html5up.net">HTML5 UP</a>.</p>
+    </header>
 )
 
 export default Header
