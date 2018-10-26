3deb7b3373407daad7e35906c4803f167b7e5175
nukosuke <nukosuke@lavabit.com>
2018-10-27 00:00:28 +0900

    Remove logo from header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,7 @@
 import React from 'react'
 
-import logo from '../assets/images/logo.svg';
-
 const Header = (props) => (
     <header id="header" className="alt">
-        <span className="logo"><img src={logo} alt="" /></span>
         <h1><b>じぶん α ver</b></h1>
         <p><b>nukosuke's web</b></p>
     </header>
