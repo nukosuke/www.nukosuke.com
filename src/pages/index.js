6b8401002a9ddeb936f9f9a9b1de071df89fc164
nukosuke <39.nuko@gmail.com>
2019-02-22 10:05:58 +0900

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import Helmet from 'react-helmet'
-import Waypoint from 'react-waypoint'
+import { Waypoint } from 'react-waypoint'
 
 import Layout from '../components/layout'
 import Header from '../components/Header'
