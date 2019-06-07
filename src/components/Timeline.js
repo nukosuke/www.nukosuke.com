ace46ae4632f31cae3311a4230d59c47b29abf71
nukosuke <nukosuke@lavabit.com>
2019-06-07 14:27:02 +0900

    Add Timeline component

---
+++ src/components/Timeline.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Timeline extends React.Component {
+  render() {
+    return (
+      <>
+        <ul className="ln-timeline">
+          { this.props.items.map(item => {
+            return (
+              <li className="ln-timeline-item">
+                <div className="ln-timeline-item-tail"></div>
+                <div className="ln-timeline-item-head ln-timeline-item-head-blue"></div>
+                <div className="ln-timeline-item-content">
+                  { item.title }
+                </div>
+              </li>
+            )
+          }) }
+        </ul>
+      </>
+    )
+  }
+}
+
+export default Timeline;
