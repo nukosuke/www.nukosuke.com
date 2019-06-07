4016e7de85a40807b867cb804615a27be3c257e9
nukosuke <nukosuke@lavabit.com>
2019-06-07 14:53:01 +0900

    Fix 190607 (#142)
    * Fix social buttons in footer
    
    * Fix form
    
    * Remove home
    
    * Fix attrs
    
    * Add Timeline component
    
    * Fix attr
    
    * import timeline
    
    * Fix style
    
    * Fix index.js
    
    * Current
    
    * Remove carrer.md

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,10 +8,8 @@ const Footer = (props) => (
         <section>
             <header className="major"><h2 className="major">Info</h2></header>
             <dl className="alt">
-                <dt><i class="fa fa-building"></i> Office</dt>
+                <dt><i className="fa fa-building"></i> Office</dt>
                 <dd>渋谷 &bull; 東京</dd>
-                <dt><i class="fa fa-home"></i> Home</dt>
-                <dd>川崎 &bull; 神奈川</dd>
             </dl>
             <ul className="icons">
                 <li><a href="https://twitter.com/pg_nukosuke" className="icon fa-twitter alt"><span className="label">Twitter</span></a></li>
@@ -22,11 +20,11 @@ const Footer = (props) => (
         <section>
           <header className="major"><h2>お問い合わせ</h2></header>
           <form action="https://formspree.io/39.nuko+www@gmail.com" method="POST">
-            <label for="email">メールアドレス</label>
+            <label htmlFor="email">メールアドレス</label>
             <input type="email" name="email" required />
-            <label for="email">本文</label>
+            <label htmlFor="email">本文</label>
             <textarea name="body" required></textarea>
-            <input type="submit" name="送信" />
+            <button type="submit">送信</button>
           </form>
         </section>
         <p className="copyright">&copy; nukosuke.com (*ΦωΦ*)</p>
