f12f4aa5b9db716cc564efc01d96350f048cdd92
nukosuke <nukosuke@users.noreply.github.com>
2022-12-15 19:19:00 +0900

    Fix

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,34 +1,40 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import { FaMastodon, FaTwitter, FaGithub, FaMedium } from 'react-icons/fa'
+import { SiMatrix, SiQiita, SiZenn } from 'react-icons/si'
 
 import logo from '../assets/images/logo.svg';
 
 const Footer = (props) => (
-    <footer id="footer">
-        <section>
-            <header className="major"><h2 className="major">Info</h2></header>
-            <dl className="alt">
-                <dt><i className="fa fa-building"></i> Office</dt>
-                <dd>渋谷 &bull; 東京</dd>
-            </dl>
-            <ul className="icons">
-                <li><a href="https://twitter.com/pg_nukosuke" className="icon fa-twitter alt"><span className="label">Twitter</span></a></li>
-                <li><a href="https://github.com/nukosuke" className="icon fa-github alt"><span className="label">GitHub</span></a></li>
-                <li><a href="https://medium.com/@nukosuke" className="icon fa-medium alt"><span className="label">Medium</span></a></li>
-            </ul>
-        </section>
-        <section>
-          <header className="major"><h2>お問い合わせ</h2></header>
-          <form action="https://formspree.io/39.nuko+www@gmail.com" method="POST">
-            <label htmlFor="email">メールアドレス</label>
-            <input type="email" name="email" required />
-            <label htmlFor="email">本文</label>
-            <textarea name="body" required></textarea>
-            <button type="submit">送信</button>
-          </form>
-        </section>
-        <p className="copyright">&copy; nukosuke.com (*ΦωΦ*)</p>
-    </footer>
+  <footer id="footer">
+    <section>
+      <header className="major"><h2 className="major">Info</h2></header>
+      <dl className="alt">
+        <dt><i className="fa fa-building"></i> Location</dt>
+        <dd>Shibuya-ku Tokyo, Japan</dd>
+      </dl>
+      <ul className="icons">
+        <li><a rel="me" href="https://mastodon.social/@nukosuke"><FaMastodon /></a></li>
+        <li><a href="https://twitter.com/pg_nukosuke"><FaTwitter /></a></li>
+        <li><a href="https://github.com/nukosuke"><FaGithub /></a></li>
+        <li><a href='https://matrix.to/#/@nukosuke:matrix.org'><SiMatrix /></a></li>
+        <li><a href="https://medium.com/@nukosuke"><FaMedium /></a></li>
+        <li><a href="https://qiita.com/nukosuke"><SiQiita /></a></li>
+        <li><a href="https://zenn.dev/nukosuke"><SiZenn /></a></li>
+      </ul>
+    </section>
+    <section>
+      <header className="major"><h2>お問い合わせ</h2></header>
+      <form action="https://formspree.io/39.nuko+www@gmail.com" method="POST">
+        <label htmlFor="email">メールアドレス</label>
+        <input type="email" name="email" required />
+        <label htmlFor="email">本文</label>
+        <textarea name="body" required></textarea>
+        <button type="submit">送信</button>
+      </form>
+    </section>
+    <p className="copyright">&copy; nukosuke.com (*ΦωΦ*)</p>
+  </footer>
 );
 
 export default Footer;
