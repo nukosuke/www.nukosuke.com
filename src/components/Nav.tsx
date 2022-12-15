bb25069a645951925ab62dd463123113b3c08aa1
nukosuke <nukosuke@users.noreply.github.com>
2022-12-15 15:56:57 +0900

    Use ts

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,23 +1,28 @@
 import React from 'react';
-import Scrollspy from 'react-scrollspy';
+import { Scrollspy } from '@makotot/ghostui';
 import Scroll from './Scroll';
 
-const Nav = (props) => (
-    <nav id="nav" className={props.sticky ? 'alt' : ''}>
-        <Scrollspy items={props.items.map(item => item.key)} currentClassName="is-active" offset={-300}>
-          {
-            props.items.map(item => {
-              return (
-                <li>
-                  <Scroll type="id" element={item.key}>
-                    <a href={`#${item.key}`}>{item.value}</a>
-                  </Scroll>
-                </li>   
-              );
-            })
-          }
-        </Scrollspy>
-    </nav>
+type NavProps = {
+  sticky: boolean
+  items: { key: string, value: string }[]
+}
+
+const Nav: React.FC<NavProps> = (props) => (
+  <nav id="nav" className={props.sticky ? 'alt' : ''}>
+    <Scrollspy items={props.items.map(item => item.key)} currentClassName="is-active" offset={-300}>
+      {
+        props.items.map(item => {
+          return (
+            <li>
+              <Scroll type="id" element={item.key}>
+                <a href={`#${item.key}`}>{item.value}</a>
+              </Scroll>
+            </li>
+          );
+        })
+      }
+    </Scrollspy>
+  </nav>
 );
 
 export default Nav;
