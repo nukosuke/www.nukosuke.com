6806f919b7c91d00f7f3f460c5b10f68c66a8c51
Yosuke Tamura <17716649+nukosuke@users.noreply.github.com>
2024-07-18 22:39:53 +0900

    Migrate Layout to React.Component to React.FC

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import * as React from 'react';
+import { useState } from 'react';
 import { Link, graphql, PageProps, HeadProps } from 'gatsby';
 import { Waypoint } from 'react-waypoint';
 
