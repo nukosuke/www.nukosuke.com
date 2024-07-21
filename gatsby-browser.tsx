e487ae07c2038473f186b6bb95f9e90f9813f1e0
Yosuke Tamura <17716649+nukosuke@users.noreply.github.com>
2024-07-21 19:36:26 +0900

    Add Chakra theme

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { ChakraProvider } from '@chakra-ui/react';
-//import { customTheme } from './src/theme'
+import { chakraTheme } from './src/theme';
 
 export const wrapRootElement = ({ element }) => (
-  <ChakraProvider>{element}</ChakraProvider>
+  <ChakraProvider theme={chakraTheme}>{element}</ChakraProvider>
 );
