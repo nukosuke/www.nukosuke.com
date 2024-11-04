b644970c560547da27bc6c546d756b9313e6f546
nukosuke <39.nuko@gmail.com>
2024-11-04 15:32:10 +0900

    Fix theme definition

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { ChakraProvider } from '@chakra-ui/react';
-import { chakraTheme } from './src/theme';
+import { system } from './src/theme';
 
 export const wrapRootElement = ({ element }) => (
-  <ChakraProvider theme={chakraTheme}>{element}</ChakraProvider>
+  <ChakraProvider value={system}>{element}</ChakraProvider>
 );
--- src/theme/chakra-theme.ts
+++ src/theme/chakra-theme.ts
@@ -1,53 +1,48 @@
-import { extendTheme } from '@chakra-ui/react';
+import { createSystem, defaultConfig } from '@chakra-ui/react';
 
-export const chakraTheme = extendTheme({
-  styles: {
-    global: {
-      body: {
-        background:
-          'linear-gradient(-45deg, #73d2f3 25%, #26baee 25%, #26baee 50%, #73d2f3 50%, #73d2f3 75%,	#26baee 75%, #26baee) !important',
-        backgroundSize: '50px 50px !important',
+export const system = createSystem(defaultConfig, {
+  globalCss: {
+    body: {
+      background:
+        'linear-gradient(-45deg, #73d2f3 25%, #26baee 25%, #26baee 50%, #73d2f3 50%, #73d2f3 75%,	#26baee 75%, #26baee) !important',
+      backgroundSize: '50px 50px !important',
+    },
+  },
+  theme: {
+    tokens: {
+      colors: {
+        yellow: {
+          50: { value: '#ffe867' },
+          60: { value: '#fff4e0' },
+        },
+        blue: {
+          30: { value: '#9bb2e1' },
+          50: { value: '#8cc9f0' },
+        },
+        purple: {
+          50: { value: '#a89cc8' },
+          60: { value: '#c79cc8' },
+        },
+        orange: {
+          50: { value: '#efa8b0' },
+          60: { value: '#e37682' },
+        },
+        gray: {
+          10: { value: '#dddddd' },
+          80: { value: '#636363' },
+          90: { value: '#5f4d93' },
+        },
       },
-    },
-  },
-  colors: {
-    yellow: {
-      50: '#ffe867',
-      60: '#fff4e0',
-    },
-    blue: {
-      30: '#9bb2e1',
-      50: '#8cc9f0',
-    },
-    purple: {
-      50: '#a89cc8',
-      60: '#c79cc8',
-    },
-    orange: {
-      50: '#efa8b0',
-      60: '#e37682',
-    },
-    gray: {
-      10: '#dddddd',
-      80: '#636363',
-      90: '#5f4d93',
-    },
-  },
-  space: {
-    'spc.4': '0.25rem',
-    'spc.6': '0.375rem',
-    'spc.8': '0.5rem',
-    'spc.12': '0.75rem',
-    'spc.16': '1rem',
-    'spc.32': '2rem',
-  },
-  sizes: {
-    'size.8': '.5rem',
-  },
-  components: {
-    Text: {
-      variants: {
-        'body.1': {},
+      spacing: {
+        'spc.4': { value: '0.25rem' },
+        'spc.6': { value: '0.375rem' },
+        'spc.8': { value: '0.5rem' },
+        'spc.12': { value: '0.75rem' },
+        'spc.16': { value: '1rem' },
+        'spc.32': { value: '2rem' },
+      },
+      sizes: {
+        'size.8': { value: '.5rem' },
       },
     },
   },
