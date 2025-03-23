9795d3fa3f1c72288bcb2922e1302ec2fb34c9c4
nukosuke <39.nuko@gmail.com>
2025-03-23 23:43:46 +0900

    Render markdown pages with mdsvex (#563)
    * Render markdown pages with mdsvex
    
    * Add layout for md doc

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,12 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { mdsvex } from 'mdsvex';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   // Consult https://svelte.dev/docs/kit/integrations
   // for more information about preprocessors
-  preprocess: vitePreprocess(),
+  preprocess: [vitePreprocess(), mdsvex({ extensions: ['.svx', '.md'] })],
 
   kit: {
     // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
@@ -13,6 +14,8 @@ const config = {
     // See https://svelte.dev/docs/kit/adapters for more information about adapters.
     adapter: adapter(),
   },
+
+  extensions: ['.svelte', '.svx', '.md'],
 };
 
 export default config;
