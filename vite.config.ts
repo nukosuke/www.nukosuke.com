5f65de6a301c91509794faa5937c6ab56fc32c8e
nukosuke <39.nuko@gmail.com>
2025-03-24 00:23:43 +0900

    Use paraglide-js v2

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,18 @@
-import { paraglide } from '@inlang/paraglide-sveltekit/vite'
 import { defineConfig } from 'vitest/config';
 import { enhancedImages } from '@sveltejs/enhanced-img';
 import { sveltekit } from '@sveltejs/kit/vite';
+import { paraglideVitePlugin } from '@inlang/paraglide-js';
 
 export default defineConfig({
-  plugins: [paraglide({ project: './project.inlang', outdir: './src/lib/paraglide' }),enhancedImages(), sveltekit()],
+  plugins: [
+    enhancedImages(),
+    sveltekit(),
+    paraglideVitePlugin({
+      project: './project.inlang',
+      outdir: './src/lib/paraglide',
+      strategy: ['url', 'baseLocale'],
+    }),
+  ],
 
   test: {
     include: ['src/**/*.{test,spec}.{js,ts}'],
